2839c9ad0044ec8aa00112e4b69f3b546ae3353b
marceloduarte <marceloduarteavila@live.com>
2019-11-22 19:53:25 -0300

    Novo arquivo

---
+++ Produtos.cs
@@ -0,0 +1,12 @@
+public class Produto
+{
+    public Produto(int Id, string Descricao)
+    {
+        this.id = Id;
+        this.descricao = Descricao;
+    }
+       private int Id { get; private set; }
+       private string Descricao { get; private set; }
+    
+
+}
