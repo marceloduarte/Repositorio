8890016655a88128fbf92869fce9a859ec1338e2
marceloduarte <marceloduarteavila@live.com>
2020-06-16 09:41:53 -0300

    novo campo

--- Produtos.cs
+++ Produtos.cs
@@ -3,11 +3,14 @@ public class Produto
     public Produto(int Id, string Descricao, string Categoria, string CodigoOriginal)
     {
         this.id = Id;
+        this.code = "";
         this.descricao = Descricao;
         this.Categoria = Categoria;
                 
     }
        private int Id { get; private set; }
+       
+       private string code { get; private set; }
        private string Descricao { get; private set; }
        private string Categoria {get ; private set;} 
        private string CodigoOriginal {get ; private set;} 
